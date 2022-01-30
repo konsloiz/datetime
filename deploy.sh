2cd9a0b7e57ee404d13c10148661eb69405880ba
konsloiz <konsloiz@gmail.com>
2022-01-30 02:32:22 +0100

    Bash script to create the kind cluster and deploy the app

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+cd Terraform/k8s
+
+echo "Creating a kind cluster"
+
+terraform init
+terraform apply -auto-approve
+
+echo "The installtion of the kind cluster is complete"
+
+echo "Deploying the datetime application"
+
+cd ../app
+
+terraform init
+terraform apply -auto-approve
+
+echo "The installtion of the datetime application is complete"
+
+cd ..
+
+cd ..
+
+echo "Initializing the health check"
+
+sleep 10
+
+echo "Health check for: http://localhost/now"
+
+python3 health_check.py --url http://localhost/now
+
+echo "Health check for: http://localhost/now/simple"
+
+python3 health_check.py --url http://localhost/now/simple
